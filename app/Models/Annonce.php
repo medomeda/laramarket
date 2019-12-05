90b5872d0b21daeae078df3bb81d95a829e4c9b6
medomeda <traore_news@yahoo.fr>
2019-12-05 12:08:12 +0000

    Création relation annonce, user, categorie

--- app/Models/Annonce.php
+++ app/Models/Annonce.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Annonce extends Model
 {
-    //
+    protected $fillable = [
+        'titre',
+        'description',
+        'categorie_id',
+        'user_id',
+        'actif',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categorie()
+    {
+        return $this->belongsTo(Categorie::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -27,4 +27,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function annonce()
+    {
+        return $this->hasMany(Annonce::class);
+    }
 }
